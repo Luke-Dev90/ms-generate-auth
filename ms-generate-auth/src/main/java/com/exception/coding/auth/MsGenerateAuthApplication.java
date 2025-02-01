1480fce54fbfa7866746b9d5eef3ad7c78c367db
Luke-Dev90 <lukee_sf@hotmail.com>
2025-01-31 22:30:55 -0300

    first version

---
+++ ms-generate-auth/src/main/java/com/exception/coding/auth/MsGenerateAuthApplication.java
@@ -0,0 +1,13 @@
+package com.exception.coding.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGenerateAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGenerateAuthApplication.class, args);
+	}
+
+}
